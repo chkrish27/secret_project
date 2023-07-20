0f3d5f7113c96226a383f48d993b4694d20d2a54
chkrish27 <venkatsai.ch16@gmail.com>
2023-07-20 07:36:53 +0530

    intial commit

--- app.js
+++ app.js
@@ -85,7 +85,22 @@ app.post("/register",async(req,res)=>{
 });
 
 app.post("/login", async (req, res) => {
-    
+    const user =new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.logIn(user,function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+               res.redirect("/secrets");
+            });
+        }
+    });
   });
 
 app.listen(3000,function(){
